0c8185f97a89fa0185741c919e9632376ea391bb
unknown321 <unknown321@users.noreply.github.com>
2022-08-27 14:56:40 +0300

    limit by host and download size

--- handler/handler.go
+++ handler/handler.go
@@ -12,20 +12,47 @@ import (
 	"golang.org/x/sync/errgroup"
 )
 
-var httpTimeout = time.Second * 5
+var (
+	httpTimeout = time.Second * 5
+)
 
-func pipe(from net.Conn, to net.Conn) error {
+type Bucket struct {
+	Quota         int64
+	Value         int64
+	LastAccess    time.Time
+	LimitDuration time.Duration
+}
+
+type Buckets map[string]Bucket
+
+func (b *Bucket) ResetQuota() {
+	b.Value = 0
+}
+
+var buckets = Buckets{"google.com:443": {
+	Quota:         2 * 1024 * 1024,
+	Value:         0,
+	LastAccess:    time.Time{},
+	LimitDuration: time.Second * 10,
+}}
+
+func pipe(from net.Conn, to net.Conn) (b int64, err error) {
 	defer from.Close()
 	n, err := io.Copy(from, to)
 	log.Printf("Wrote: %d bytes", n)
 
 	if err != nil && strings.Contains(err.Error(), "closed network") {
-		return nil
+		return n, nil
 	}
 
-	return fmt.Errorf("cannot pipe: %w", err)
+	if err != nil {
+		return 0, fmt.Errorf("cannot pipe: %w", err)
+	}
+
+	return n, nil
 }
 
+//nolint:funlen
 func Handle() http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if r.Method != http.MethodConnect {
@@ -34,7 +61,18 @@ func Handle() http.Handler {
 			return
 		}
 
-		log.Printf("%s", r.Host)
+		if v, ok := buckets[r.Host]; ok {
+			if time.Now().Add(v.LimitDuration * -1).After(v.LastAccess) {
+				log.Printf("Quota reset for %s", r.Host)
+				v.ResetQuota()
+			}
+
+			if v.Value > v.Quota {
+				http.Error(w, "Quota reached", http.StatusPaymentRequired)
+
+				return
+			}
+		}
 
 		defer r.Body.Close()
 
@@ -66,16 +104,33 @@ func Handle() http.Handler {
 
 		g := new(errgroup.Group)
 		g.Go(func() error {
-			return pipe(reqConn, conn)
+			return pipeWithBucket(reqConn, conn, r.Host)
 		})
 		g.Go(func() error {
-			return pipe(conn, reqConn)
+			_, errReq := pipe(conn, reqConn)
+
+			return errReq
 		})
 
-		if err := g.Wait(); err != nil {
+		if err = g.Wait(); err != nil {
 			log.Println("Error", err.Error())
 		}
 
 		log.Printf("Connection %s done.", conn.RemoteAddr())
 	})
+}
+
+func pipeWithBucket(reqConn net.Conn, conn net.Conn, host string) error {
+	b, errResp := pipe(reqConn, conn)
+	if b != 0 {
+		if v, ok := buckets[host]; ok {
+			v.Value += b
+			v.LastAccess = time.Now()
+			buckets[host] = v
+
+			log.Printf("host %s, value %d, quota %d", host, buckets[host].Value, buckets[host].Quota)
+		}
+	}
+
+	return errResp
 }
